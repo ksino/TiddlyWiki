e6ff54fffdb002a5e35cfb16b924b6e9ef37dbd6
ksino <30484235+ksino@users.noreply.github.com>
2021-01-24 16:47:13 +0800

    modify bulidWiki.js 3

--- scripts/buildWiki.js
+++ scripts/buildWiki.js
@@ -6,7 +6,7 @@ const wikiFolderName = require('../package.json').name;
 const repoFolder = path.join(path.dirname(__filename), '..');
 
 const tiddlyWikiFolder = path.join(repoFolder, wikiFolderName);
-console.log("---------tiddlyWikiFolder", tiddlyWikiFolder)
+// console.log("---------tiddlyWikiFolder", tiddlyWikiFolder)
 // cross-env TIDDLYWIKI_PLUGIN_PATH='node_modules/tiddlywiki/plugins/published' TIDDLYWIKI_THEME_PATH='${wikiFolderName}/themes'
 process.env['TIDDLYWIKI_PLUGIN_PATH'] = `${tiddlyWikiFolder}/plugins`;
 process.env['TIDDLYWIKI_THEME_PATH'] = `${tiddlyWikiFolder}/themes`;
@@ -16,27 +16,34 @@ const execAndLog = (command, options) => console.log(String(execSync(command, op
 module.exports = function build() {
   // npm run build:prepare
   execAndLog(`rm -rf ${repoFolder}/public`);
+  console.log("-------------------------1")
   // npm run build:public
   execAndLog(
     `cp -r ${tiddlyWikiFolder}/public/ ./public`,
     { cwd: repoFolder }
   );
+  console.log("-------------------------2")
   // npm run build:nodejs2html
   execAndLog(`tiddlywiki ${wikiFolderName} --build externalimages`, { cwd: repoFolder });
+  console.log("-------------------------3")
   // npm run build:sitemap
   execAndLog(
     `tiddlywiki ${wikiFolderName} --rendertiddler sitemap sitemap.xml text/plain && mv ${tiddlyWikiFolder}/output/sitemap.xml ./public/sitemap.xml`,
     { cwd: repoFolder }
   );
+  console.log("-------------------------4")
   // npm run build:minifyHTML
   execAndLog(
     `html-minifier-terser -c ./html-minifier-terser.config.json -o ./public/index.html ${tiddlyWikiFolder}/output/index.html`,
     { cwd: repoFolder }
   );
+  console.log("-------------------------5")
   // npm run build:precache
   execAndLog(`workbox injectManifest workbox-config.js`, { cwd: repoFolder });
+  console.log("-------------------------6")
   // npm run build:clean
   execAndLog(`rm -r ${tiddlyWikiFolder}/output`, { cwd: repoFolder });
+  console.log("-------------------------7")
   // npm run build:pluginLibrary
   // execAndLog(`tiddlywiki ${wikiFolderName} --output public/library --build library`, { cwd: repoFolder });
 };
