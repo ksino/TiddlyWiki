ea5cdc9e5394d7d9dd9f74643a2bd5c78bc72f8a
ksino <ksino@users.noreply.github.com>
2021-01-25 05:22:01 +0000

    deploy: 292203c057c23d6cc33c8128afc78e3140369ca9

---
+++ service-worker.js
@@ -0,0 +1,42 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.3/workbox-sw.js');
+
+if (workbox) {
+  console.log(`Yay! Workbox is loaded 🎉Service Worker is working!`);
+} else {
+  console.log(`Boo! Workbox didn't load 😬Service Worker won't work properly...`);
+}
+
+const { registerRoute } = workbox.routing;
+const { CacheFirst, StaleWhileRevalidate } = workbox.strategies;
+const { ExpirationPlugin } = workbox.expiration;
+const { precacheAndRoute, matchPrecache } = workbox.precaching;
+
+precacheAndRoute([{"revision":"a77fee17dc23530a51ca9b1f1cd1b2ca","url":"favicon.ico"},{"revision":"e9e70bb081bb4e3f23b0bc597b7ff25b","url":"index.html"},{"revision":"a77fee17dc23530a51ca9b1f1cd1b2ca","url":"TiddlyWikiIconBlack.png"},{"revision":"a77fee17dc23530a51ca9b1f1cd1b2ca","url":"TiddlyWikiIconWhite.png"}]);
+
+registerRoute(
+  /\.css$/,
+  // Use cache but update in the background.
+  new StaleWhileRevalidate({
+    // Use a custom cache name.
+    cacheName: 'css-cache',
+  })
+);
+
+registerRoute(
+  /\.(?:png|jpg|jpeg|svg|gif|woff2?|ttf)$/,
+  // Use the cache if it's available.
+  new CacheFirst({
+    cacheName: 'image-cache',
+    plugins: [
+      new ExpirationPlugin({
+        // Cache only a few images.
+        maxEntries: 100,
+        // Cache for a maximum of a week.
+        maxAgeSeconds: 7 * 24 * 60 * 60,
+      }),
+    ],
+  })
+);
+
+registerRoute(/\.js$/, new StaleWhileRevalidate());
+registerRoute(/(^\/$|index.html)/, new StaleWhileRevalidate());
