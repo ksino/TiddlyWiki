292203c057c23d6cc33c8128afc78e3140369ca9
ksino <flycat86@163.com>
2021-01-25 13:20:58 +0800

    debug auto build index.html

--- scripts/buildWiki.js
+++ scripts/buildWiki.js
@@ -6,7 +6,7 @@ const wikiFolderName = require('../package.json').name;
 const repoFolder = path.join(path.dirname(__filename), '..');
 
 const tiddlyWikiFolder = path.join(repoFolder, wikiFolderName);
-// console.log("---------tiddlyWikiFolder", tiddlyWikiFolder)
+
 // cross-env TIDDLYWIKI_PLUGIN_PATH='node_modules/tiddlywiki/plugins/published' TIDDLYWIKI_THEME_PATH='${wikiFolderName}/themes'
 process.env['TIDDLYWIKI_PLUGIN_PATH'] = `${tiddlyWikiFolder}/plugins`;
 process.env['TIDDLYWIKI_THEME_PATH'] = `${tiddlyWikiFolder}/themes`;
@@ -15,35 +15,32 @@ const execAndLog = (command, options) => console.log(String(execSync(command, op
 
 module.exports = function build() {
   // npm run build:prepare
+  // 删除public文件夹
   execAndLog(`rm -rf ${repoFolder}/public`);
-  console.log("-------------------------1")
+
   // npm run build:public
   execAndLog(
-    `cp -r ${tiddlyWikiFolder}/public/ ./public`,
+    `cp -r ${tiddlyWikiFolder}/public/ ./public && cp ${tiddlyWikiFolder}/tiddlers/favicon.ico ./public/favicon.ico && cp ${tiddlyWikiFolder}/tiddlers/TiddlyWikiIconWhite.png ./public/TiddlyWikiIconWhite.png && cp ${tiddlyWikiFolder}/tiddlers/TiddlyWikiIconBlack.png ./public/TiddlyWikiIconBlack.png`,
     { cwd: repoFolder }
   );
-  console.log("-------------------------2")
+
   // npm run build:nodejs2html
+  // 需要使用原来github模板的tiddlywiki.info配置
   execAndLog(`tiddlywiki ${wikiFolderName} --build externalimages`, { cwd: repoFolder });
-  console.log("-------------------------3")
   // npm run build:sitemap
   execAndLog(
     `tiddlywiki ${wikiFolderName} --rendertiddler sitemap sitemap.xml text/plain && mv ${tiddlyWikiFolder}/output/sitemap.xml ./public/sitemap.xml`,
     { cwd: repoFolder }
   );
-  console.log("-------------------------4")
   // npm run build:minifyHTML
   execAndLog(
     `html-minifier-terser -c ./html-minifier-terser.config.json -o ./public/index.html ${tiddlyWikiFolder}/output/index.html`,
     { cwd: repoFolder }
   );
-  console.log("-------------------------5")
   // npm run build:precache
   execAndLog(`workbox injectManifest workbox-config.js`, { cwd: repoFolder });
-  console.log("-------------------------6")
   // npm run build:clean
   execAndLog(`rm -r ${tiddlyWikiFolder}/output`, { cwd: repoFolder });
-  console.log("-------------------------7")
   // npm run build:pluginLibrary
   // execAndLog(`tiddlywiki ${wikiFolderName} --output public/library --build library`, { cwd: repoFolder });
 };
